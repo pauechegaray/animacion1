0a82710dcd9baf304f7d13ac8b750c11da238a65
Ivan <a@b.cd>
2018-03-12 10:13:07 -0500

    Tweak duration

--- src/js/components/intro.js
+++ src/js/components/intro.js
@@ -125,7 +125,7 @@ const tween = TweenMax.to(obj, 0.5,
   }
 );
 
-const imagesScene = new ScrollMagic.Scene({triggerElement: ".intro__trigger", duration: "500%"})
+const imagesScene = new ScrollMagic.Scene({triggerElement: ".intro__trigger", duration: "750%"})
 .setTween(tween)
 .addTo(controller);
 
